520c037c1f7cb85917f9ae9419efd05836ef08cd
LupusXLass1404 <berkasxlupusxlass@gmail.com>
2024-11-26 14:10:20 +0800

    在類別內建立取出單筆和多筆資料的共用函式

---
+++ object/db.php
@@ -0,0 +1,57 @@
+<?php
+
+class DB{
+    protected $dsn="mysql:host=localhost;charsrt=utf8;dbname=db99";
+    protected $pdo;
+    protected $table;
+
+    function __construct($table){
+        $this->table=$table;
+        $this->pdo=new PDO($this->dsn,'root','');
+    }
+
+    /**
+     * 撈出全部資料
+     */
+
+    function all(){
+        return $this->q("SELECT * FROM $this->table");
+    }
+
+    /**
+     * 把陣列轉成條件字串陣列
+     */
+
+     function toWhere($array){
+        $tmp=[];
+        foreach($array as $key => $value){
+            $tmp[] = "`$key` = '$value'";
+        }
+        return $tmp;
+     }
+
+    function fetchOne(){
+        //echo $sql;
+        return $this->pdo->query($sql)->fetch();
+    }
+    
+    function fetchAll(){
+        //echo $sql;
+        return $this->pdo->query($sql)->fetchAll();
+     }
+}
+
+// function q($sql){
+//     return $this->pdo->query($sql)->fetchAll();
+// }
+
+function dd($array){
+    echo "<pre>";
+    print_r($array);
+    echo "</pre>";
+}
+
+$DEPT=new DB('dept');
+$dept=$DEPT->all();
+
+dd($dept);
