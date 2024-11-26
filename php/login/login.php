f6988321fadb978dabd592edd0a716e463efb133
LupusXLass1404 <berkasxlupusxlass@gmail.com>
2024-11-26 11:28:26 +0800

    建立一個讀取資料的物件

---
+++ php/login/login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="./loginCheck.php" method="post">
+        <p>
+            <label for="acc">帳號：</label>
+            <input type="text" name="acc" id="">
+        </p>
+        <p>
+            <label for="">密碼：</label>
+            <input type="text" name="pw" id="">
+        </p>
+        <input type="submit" value="登入">
+    </form>
+    
+</body>
+</html>
---
+++ php/login/loginCheck.php
@@ -0,0 +1,10 @@
+<?php
+
+$acc = $_POST['acc'] ?? null;
+$pw = $_POST['pw'] ?? null;
+
+
+echo $acc;
+echo $pw;
+
+?>
